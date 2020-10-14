10fdcb9789453be4786f937b282f8d00d4886724
Old <onelifedesigning@gmail.com>
2020-10-14 20:02:10 +0200

    DONE

--- index.js
+++ index.js
@@ -1,41 +1,34 @@
-export const URLSlug = (words) => (
-  words
-    .replace(/\s+/g, '-')
-    .toLowerCase()
-);
+export const URLSlug = (words) => words.replace(/\s+/g, "-").toLowerCase();
 
-export const initials = (name) => (
+export const initials = (name) =>
   name
-    .split(' ')
-    .map(word => `${word.charAt(0).toUpperCase()}.`)
-    .join('')
-);
+    .split(" ")
+    .map((word) => `${word.charAt(0).toUpperCase()}.`)
+    .join("");
 
 const months = [
-  'january',
-  'february',
-  'march',
-  'april',
-  'may',
-  'june',
-  'july',
-  'august',
-  'september',
-  'october',
-  'november',
-  'december'
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december",
 ];
 
-export const monthName = (place) => (
-  months[place]
-    ? months[place]
-    : 'There are only 12 months!'
-);
+export const monthName = (place) =>
+  months[place] ? months[place] : "There are only 12 months!";
 
-export const hasPrefix = (str, prefix) => str.startsWith(prefix)
+export const hasPrefix = (str, prefix) => str.startsWith(prefix);
 
-export const withTwoDecimals = (num) => Math.round(100 * num) / 100
+export const withTwoDecimals = (num) => Math.round(100 * num) / 100;
 
-export const twoDigitsNum = num => `0${num}`.substr(-2)
+export const twoDigitsNum = (num) => `0${num}`.substr(-2);
 
-export const removeDuplicates = arr => arr.filter((item, i) => arr.indexOf(item) === i)
+export const removeDuplicates = (arr) =>
+  arr.filter((item, i) => arr.indexOf(item) === i);
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,29 +1,60 @@
-import { URLSlug, hasPrefix, initials, monthName, twoDigitsNum, withTwoDecimals, removeDuplicates } from '../index'
+import {
+  URLSlug,
+  hasPrefix,
+  initials,
+  monthName,
+  twoDigitsNum,
+  withTwoDecimals,
+  removeDuplicates,
+} from "../index";
 
-describe('URLSlug', () => {
-  
-})
+describe("URLSlug", () => {
+  test("Convert a paragraph to lowercase and replace the spaces with a dash", () => {
+    expect(URLSlug("Hola que ase")).toBe("hola-que-ase");
+  });
+});
 
-describe('initials', () => {
+describe("initials", () => {
+  test("Converts a separate full name initials points", () => {
+    expect(initials("Manolo Pérez Canto")).toBe("M.P.C.");
+  });
+});
 
-})
+describe("monthName", () => {
+  test("Take number month and return name", () => {
+    expect(monthName(1)).toBe("february");
+  });
+  test("Take nmber month and return name only if minor that 11", () => {
+    expect(monthName(12)).toBe("There are only 12 months!");
+  });
+});
 
-describe('monthName', () => {
+describe("hasPrefix", () => {
+  test("Check if the word starts with the letter ", () => {
+    expect(hasPrefix("Hola", "H")).toBeTruthy();
+  });
+  test("Check if not the word starts with the letter ", () => {
+    expect(hasPrefix("Hola", "P")).toBeFalsy();
+  });
+});
 
-})
+describe("withTwoDecimals", () => {
+  test("Convert to rounded decimals", () => {
+    expect(withTwoDecimals(25.9966)).toBe(26);
+  });
+});
 
-describe('hasPrefix', () => {
+describe("twoDigitsNum", () => {
+  test("Convert to string whith 0", () => {
+    expect(twoDigitsNum(1)).toBe("01");
+  });
+  test("Convert to string whith 0 only if bigest", () => {
+    expect(twoDigitsNum(11)).toBe("11");
+  });
+});
 
-})
-
-describe('withTwoDecimals', () => {
-
-})
-
-describe('twoDigitsNum', () => {
-
-})
-
-describe('removeDuplicates', () => {
-
-})
+describe("removeDuplicates", () => {
+  test("Remove items duplicated on array", () => {
+    expect(removeDuplicates([2, 3, 4, 2])).toEqual([2, 3, 4]);
+  });
+});
