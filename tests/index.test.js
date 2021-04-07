cb5826c2f948979554e84698049aa2cddab21618
Javier Repi <xavirepi@hotmail.com>
2021-04-07 14:32:13 +0200

    add sum and URLSlug tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,36 @@
+import { TestScheduler } from '@jest/core'
 import { URLSlug, hasPrefix, initials, monthName, twoDigitsNum, withTwoDecimals, removeDuplicates } from '../index'
 
+describe('sum', () => {
+	test('adds 2+2 to equal 4', () => {
+		expect(2 + 2).toBe(4)
+	})
+	test('adds 205 + 400 to equal 605', () => {
+		expect(205 + 400).toBe(605)
+	})
+	test('adds 1000 + 1000 to equal 2000', () => {
+		expect(1000 + 1000).toBe(2000)
+	})
+})
+
+URLSlug('W O R D S')
+
 describe('URLSlug', () => {
-  
+	test('(W O R D S) string is returned as w-o-r-d-s', () => {
+		const URLSlug = jest.fn(words => words.replace(/\s+/g, '-').toLowerCase())
+
+		URLSlug('W O R D S')
+	
+		expect(URLSlug).toHaveReturnedWith('w-o-r-d-s')
+	})
+
+	test('(sOmE URL) string is returned as some-url', () => {
+		const URLSlug = jest.fn(words => words.replace(/\s+/g, '-').toLowerCase())
+		
+		URLSlug('sOmE URL')
+	
+		expect(URLSlug).toHaveReturnedWith('some-url')
+	})
 })
 
 describe('initials', () => {
