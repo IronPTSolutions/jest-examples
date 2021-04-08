a24da70e3c1c513a323725f1b5a905fedca5037f
Javier Repi <xavirepi@hotmail.com>
2021-04-08 17:52:54 +0200

    add tests to all cases but monthName

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,58 +1,84 @@
 import { TestScheduler } from '@jest/core'
-import { URLSlug, hasPrefix, initials, monthName, twoDigitsNum, withTwoDecimals, removeDuplicates } from '../index'
+import { sum, URLSlug, hasPrefix, initials, monthName, twoDigitsNum, withTwoDecimals, removeDuplicates } from '../index'
 
 describe('sum', () => {
-	test('adds 2+2 to equal 4', () => {
-		expect(2 + 2).toBe(4)
+	test('adds 2+1 to equal 3', () => {
+		expect(sum(2,1)).toBe(3)
 	})
+
 	test('adds 205 + 400 to equal 605', () => {
-		expect(205 + 400).toBe(605)
-	})
-	test('adds 1000 + 1000 to equal 2000', () => {
-		expect(1000 + 1000).toBe(2000)
+		expect(sum(205, 400)).toBe(605)
 	})
 })
 
-URLSlug('W O R D S')
-
 describe('URLSlug', () => {
-	test('(W O R D S) string is returned as w-o-r-d-s', () => {
-		const URLSlug = jest.fn(words => words.replace(/\s+/g, '-').toLowerCase())
-
-		URLSlug('W O R D S')
-	
-		expect(URLSlug).toHaveReturnedWith('w-o-r-d-s')
+	test('"W O R D S" string is returned as w-o-r-d-s', () => {
+		expect(URLSlug('W O R D S')).toBe('w-o-r-d-s')
 	})
 
-	test('(sOmE URL) string is returned as some-url', () => {
-		const URLSlug = jest.fn(words => words.replace(/\s+/g, '-').toLowerCase())
-		
-		URLSlug('sOmE URL')
-	
-		expect(URLSlug).toHaveReturnedWith('some-url')
+	test('"sOmE URL" string is returned as some-url', () => {
+		expect(URLSlug('sOmE URL')).toBe('some-url')
 	})
 })
 
 describe('initials', () => {
+	// test('input must be a string', () => {
+	// 	//
+	// })
 
+	test('Should return initial of name uppercased', () => {
+		expect(initials('javier')).toBe('J.')
+	})
 })
 
-describe('monthName', () => {
+describe('monthName', () => {  
+	// test('Should return march', () => {
+	// 	expect(monthName('march')).toBe('march')
+	// })
 
+	// test('Throw error', () => {
+	// 	expect(monthName()).toThrow('There are only 12 months!')
+	// })
 })
 
 describe('hasPrefix', () => {
+	test('"Jav" is the prefix of string "Javier" should be true', () => {
+		expect(hasPrefix('Javier', 'Jav')).toBe(true)
+	})
 
+	test('Should return false when prefix does not match a string', () => {
+		expect(hasPrefix('Javier', 'vaJ')).toBe(false)
+	})
 })
 
 describe('withTwoDecimals', () => {
+	test('10.02203 should be 10.02', () => {
+		expect(withTwoDecimals(10.02203)).toBeCloseTo(10.02)
+	})
 
+	test('2345.39359758324 should be 10.02', () => {
+		expect(withTwoDecimals(2345.39359758324)).toBeCloseTo(2345.39)
+	})
 })
 
 describe('twoDigitsNum', () => {
+	// How to test type of input?
+	// test('input should of type number', () => {
+	// 	expect(twoDigitsNum('some string')).toThrow(undefined);
+	// })
 
+	test('2 must be 02', () => {
+		expect(twoDigitsNum(2)).toBe('02')
+	})
+
+	test('20 must be 20', () => {
+		expect(twoDigitsNum(20)).toBe('20')
+	})
 })
 
 describe('removeDuplicates', () => {
-
+	test('Should remove duplicates from array', () => {
+		const duplicates = ['Javier', 'Pepe', 'Paco', 'Javier']
+		expect(removeDuplicates(duplicates)).toEqual(['Javier', 'Pepe', 'Paco'])
+	})
 })
