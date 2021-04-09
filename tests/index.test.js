29ab0ab60c844cb2c6c6e2424d9a041338321951
Javier Repi <xavirepi@hotmail.com>
2021-04-09 22:19:14 +0200

    finish tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,41 +4,44 @@ import { sum, URLSlug, hasPrefix, initials, monthName, twoDigitsNum, withTwoDeci
 describe('sum', () => {
 	test('adds 2+1 to equal 3', () => {
 		expect(sum(2,1)).toBe(3)
-	})
-
-	test('adds 205 + 400 to equal 605', () => {
 		expect(sum(205, 400)).toBe(605)
 	})
 })
 
 describe('URLSlug', () => {
-	test('"W O R D S" string is returned as w-o-r-d-s', () => {
+	test('should remove spaces and uppercaseLetters', () => {
 		expect(URLSlug('W O R D S')).toBe('w-o-r-d-s')
+		expect(URLSlug('sOmE URL')).toBe('some-url')
 	})
 
-	test('"sOmE URL" string is returned as some-url', () => {
-		expect(URLSlug('sOmE URL')).toBe('some-url')
+	test('an input word should be lowercased', () => {
+		expect(URLSlug('IRONHACK')).toBe('ironhack')
+		expect(URLSlug('Programmer')).toBe('programmer')
 	})
 })
 
 describe('initials', () => {
-	// test('input must be a string', () => {
-	// 	//
-	// })
-
 	test('Should return initial of name uppercased', () => {
 		expect(initials('javier')).toBe('J.')
+		expect(initials('JAVIER REPILADO')).toBe('J.R.')
+	})
+	
+	test('input must be a string', () => {
+		expect(() => initials(2)).toThrow('')
 	})
 })
 
 describe('monthName', () => {  
-	// test('Should return march', () => {
-	// 	expect(monthName('march')).toBe('march')
-	// })
+	test('it returns the month name', () => {
+		expect(monthName(0)).toBe('january')
+		expect(monthName(2)).toBe('march')
+		expect(monthName(5)).toBe('june')
+		expect(monthName(11)).toBe('december')
+	})
 
-	// test('Throw error', () => {
-	// 	expect(monthName()).toThrow('There are only 12 months!')
-	// })
+	test('it should throw an error', () => {
+		expect(() => monthName(12)).toThrow('There are only 12 months!')
+	})
 })
 
 describe('hasPrefix', () => {
@@ -52,28 +55,18 @@ describe('hasPrefix', () => {
 })
 
 describe('withTwoDecimals', () => {
-	test('10.02203 should be 10.02', () => {
+	test('Should return number rounded to 2 decimals', () => {
 		expect(withTwoDecimals(10.02203)).toBeCloseTo(10.02)
-	})
-
-	test('2345.39359758324 should be 10.02', () => {
 		expect(withTwoDecimals(2345.39359758324)).toBeCloseTo(2345.39)
 	})
 })
 
 describe('twoDigitsNum', () => {
-	// How to test type of input?
-	// test('input should of type number', () => {
-	// 	expect(twoDigitsNum('some string')).toThrow(undefined);
-	// })
-
-	test('2 must be 02', () => {
+	test('It should return a two-number string', () => {
 		expect(twoDigitsNum(2)).toBe('02')
-	})
-
-	test('20 must be 20', () => {
 		expect(twoDigitsNum(20)).toBe('20')
 	})
+
 })
 
 describe('removeDuplicates', () => {
